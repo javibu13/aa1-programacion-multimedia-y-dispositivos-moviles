40c2e96f8bc283882d1c2acb7a8adaec7cc78da6
Javibu13 <javiersanzmoline@gmail.com>
2025-05-21 00:29:20 +0200

    Add API methods to services

--- app/src/main/java/com/sanvalero/android/service/PersonService.java
+++ app/src/main/java/com/sanvalero/android/service/PersonService.java
@@ -6,13 +6,25 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface PersonService {
     @GET("persons")
     Call<List<Person>> getPersons();
 
+    @GET("persons/{idPerson}")
+    Call<Person> getPersonById(@Path("idPerson") int idPerson);
+
     @POST("persons")
     Call<Person> createPerson(@Body Person person);
+
+    @PUT("persons/{idPerson}")
+    Call<Person> updatePersonById(@Path("idPerson") int idPerson, @Body Person person);
+
+    @DELETE("persons/{idPerson}")
+    Call<Void> deletePersonById(@Path("idPerson") int idPerson);
 }
--- app/src/main/java/com/sanvalero/android/service/PlaceService.java
+++ app/src/main/java/com/sanvalero/android/service/PlaceService.java
@@ -6,13 +6,25 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface PlaceService {
     @GET("places")
     Call<List<Place>> getPlaces();
 
+    @GET("places/{idPlace}")
+    Call<Place> getPlaceById(@Path("idPlace") int idPlace);
+
     @POST("places")
     Call<Place> createPlace(@Body Place place);
+
+    @PUT("places/{idPlace}")
+    Call<Place> updatePlaceById(@Path("idPlace") int idPlace, @Body Place place);
+
+    @DELETE("places/{idPlace}")
+    Call<Void> deletePlaceById(@Path("idPlace") int idPlace);
 }
